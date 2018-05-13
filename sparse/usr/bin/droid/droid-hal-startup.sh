5345a79d633e05316d21b9b65d4d5d012d2a094d
Zhang Xingtao <xingtao.zhang@yahoo.com>
2018-05-13 17:29:37 +0800

    Fix exec boot issue by adding nohup
    borrowed from @ghosalmartin
    
    Change-Id: I2d6d923569f89b5253c5e82acb415a261f74f10f

---
+++ sparse/usr/bin/droid/droid-hal-startup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+cd /
+touch /dev/.coldboot_done
+
+if [ "$(uname -m | grep -o 64)" == "64" ]; then
+    # for 64 bit use the default LD_LIBRARY_PATH, otherwise we get conflicts.
+    export LD_LIBRARY_PATH=
+else
+    # for 32 bit, this is safe
+    export LD_LIBRARY_PATH=/usr/libexec/droid-hybris/system/lib/:/vendor/lib:/system/lib
+fi
+
+# Save systemd notify socket name to let droid-init-done.sh pick it up later
+echo $NOTIFY_SOCKET > /run/droid-hal/notify-socket-name
+
+exec nohup /sbin/droid-hal-init
+
